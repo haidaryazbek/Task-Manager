d0db77eb956e4da4bbbebd0f8f1f866a7b96ab19
haidaryazbek <haidaryazbek66@gmail.com>
2023-11-10 23:55:34 +0200

    one last touch

--- scripts/index.js
+++ scripts/index.js
@@ -14,10 +14,24 @@ addtask.addEventListener("click",function(){
         tasks.innerHTML +=`            <div class="task">
         <input type="text" value="`+input.value+`" class="text" id="text" disabled/>
         <input type="checkbox" class="checkbox"/>
-        <button id="edit" class="edit" onclick="edit()">edit</button>
+        <button id="edit" class="edit" onclick="edit(this.parentElement)">edit</button>
         <button id="delete" class="delete" onclick="this.parentElement.parentElement.removeChild(this.parentElement)">X</button>
     </div>`;
     input.value="";
     }
 });
+function edit(task){
+    const text=task.getElementsByTagName("input");
+    const edit=task.getElementsByTagName("button")
+    if(text[0].disabled){
+        text[0].disabled=false;
+        text[0].style="background-color:white;";
+    }
+    else{
+        text[0].disabled=true;
+        text[0].style="background-color:rgb(219, 166, 250);";
+
+
+    }
+}
 
